b9fa912f220ac1ab1f433ffb39a7622f1781a90c
Jordan Adams <Jordan@Jordan-Adamss-MacBook-Pro.local>
2014-06-30 22:28:48 -0500

    Docstrings and component edits.

--- landlab/components/fire_generator/examples/fire_test_driver.py
+++ landlab/components/fire_generator/examples/fire_test_driver.py
@@ -1,26 +1,37 @@
-#import os
+""" fire_test_driver.py
+
+Sample driver for the generate_fire component and FireGenerator class.
+
+This file will use the default parameters to find the scale parameter for
+the fire distribution. 
+
+The fire time series is generated using that scale and shape parameters.
+The histogram is sorted and plotted to verify the Weibull distribution.
+
+Written by Jordan Marie Adams, 2013. 
+"""
+import os
 from landlab.components.fire_generator.generate_fire import FireGenerator
 from matplotlib import pyplot as plt
-#filename = '/Users/Jordan/landlab/landlab/components/uniform_precip/examples/fireraininput.txt'
 
+# Input text file name and location
+filename = os.path.join(os.path.dirname(__file__), 'fire.txt')
 
+# Initializing the FireGenerator() class from generate_fire.py'
+FG = FireGenerator(filename)
 
-print 'Initializing the FireGenerator() class from fire_generator.py'
-FG = FireGenerator()
-FG.initialize()
-
-print 'Finding unknown scale parameter given the mean fire recurrence value'
+# Finding the unknown scale parameter for the distribution
 FG.get_scale_parameter()
 
-print 'Finding a time series based on the Weibull distribution'
+# Finding a time series based on the Weibull distribution
 FG.generate_fire_time_series()
 
-print 'Sorting the fire series to test the distribution...'
-FG.fires.sort()
+# Sorting the fire series to test the distribution...
+FG.fire_events.sort()
 
-print 'Plotting the histogram to show the distribution.'
+# Plotting the histogram to show the distribution.
 ymax = int(FG.total_run_time)+1
 yaxis = range(0, ymax)
-plt.plot(FG.fires, yaxis)
-plt.hist(FG.fires)
+#plt.plot(FG.fire_events)
+plt.hist(FG.fire_events)
 plt.show()
